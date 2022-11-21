80c4fb4cfa858cf827438e870fb26898ee62b4f2
danpoggetti <danpoggetti@gmail.com>
2022-11-20 16:08:37 -0800

    added weather API keys and URL

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+let weatherAPIKey = '&appid=bf0591a4f4a4606b14032926c3f66d98';
+let weatherCall = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+let iconURL = 'https://openweathermap.org/img/w/';
